f285c14630f34632476f1ee77c3af51de2d82017
prabath <prabath@wso2.com>
2019-07-09 18:12:27 -0700

    updates

--- src/unzip.sh
+++ src/unzip.sh
@@ -2,5 +2,12 @@ for file in ./**/*.zip
 do
   unzip -d "../unzipped" "$file"
 done
-
-find ../unzipped -type f ! -name '*.jar' -delete
+cd ../unzipped
+find .  -type f ! -name '*.jar' -delete
+tree -if > ../wso2is-repo-explorer/src/patch.tree
+cd ..
+rm -rf unzipped
+cd wso2is-repo-explorer
+git add .
+git commit -m "updates"
+git push

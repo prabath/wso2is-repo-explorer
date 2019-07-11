0cfd2ef887fa696feb62e587bd9cfb01e718b208
prabath <prabath@wso2.com>
2019-07-11 12:02:37 -0700

    updates to indexes

--- src/update-indexes.sh
+++ src/update-indexes.sh
@@ -2,7 +2,7 @@ cd /Users/prabath/wso2is-repo-explorer/svn/updates
 svn up
 for file in ./**/*.zip
 do
-  unzip -d "../unzipped" "$file"
+  unzip -o -d "../unzipped" "$file"
 done
 cd ../unzipped
 find .  -type f ! -name '*.jar' -delete
@@ -10,8 +10,8 @@ tree -if > ../../git/wso2is-repo-explorer/src/indexes/updates
 cd ..
 rm -rf unzipped
 cd ../git/wso2is-repos
-./rex update
-cp .repodata/wso2 ../wso2is-repo-explorer/src/*
+./rex.sh update
+cp -r .repodata/wso2* ../wso2is-repo-explorer/src/
 cd ../wso2is-repo-explorer
 git pull
 git add .

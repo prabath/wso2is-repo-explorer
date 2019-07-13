00fb0c05b0291026340b5a68d74e57918532d2b8
prabath <prabath@wso2.com>
2019-07-12 22:09:52 -0700

    updates

--- src/update-indexes.sh
+++ src/update-indexes.sh
@@ -7,6 +7,7 @@ do
   unzip -o -d "../unzipped" "$file"
 done
 cd ../unzipped
+tree -if | grep "authenticationendpoint.war|oauth2.war|wso2.war|accountrecoveryendpoint.war|api#identity#consent-mgt#v1.0.war|api#identity#entitlement.war|api#identity#oauth2#dcr#v1.0.war|api#identity#oauth2#v1.0.war|api#identity#recovery#v0.9.war|api#identity#user#v1.0.war|emailotpauthenticationendpoint.war|scim2.war|smsotpauthenticationendpoint.war|x509certificateauthenticationendpoint.war" > ../../git/wso2is-repo-explorer/src/indexes/updates.war
 find .  -type f ! -name '*.jar' -delete
 tree -if | grep ".jar" > ../../git/wso2is-repo-explorer/src/indexes/updates
 cd ..

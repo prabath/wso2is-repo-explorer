1e25b9701e971e7dd8ad707dbd07347323c23a68
prabath <prabath@wso2.com>
2019-07-14 13:38:09 -0700

    automatic updates to indexes

--- src/update-indexes.sh
+++ src/update-indexes.sh
@@ -1,23 +1,23 @@
 cd /Users/prabath/wso2is-repo-explorer/git/wso2is-repo-explorer
-touch update.index.log
+touch /Users/prabath/wso2is-repo-explorer/update.index.log
 git pull
 cd ../../svn/updates
 svn up
- printf '%s %s\n' "$(date)" " svn updated successfully" >> update.index.log
+ printf '%s %s\n' "$(date)" " svn updated successfully" >> /Users/prabath/wso2is-repo-explorer/update.index.log
 for file in ./**/*.zip
 do
   unzip -o -d "../unzipped" "$file"
 done
-printf '%s %s\n' "$(date)" " files unzipped successfully" >> update.index.log
+printf '%s %s\n' "$(date)" " files unzipped successfully" >> /Users/prabath/wso2is-repo-explorer/update.index.log
 cd ../unzipped
 find .  -type f ! -name '*.jar' -delete
 tree -if | grep ".jar" > ../../git/wso2is-repo-explorer/src/indexes/updates
 cd ..
 rm -rf unzipped
-printf '%s %s\n' "$(date)" " removed unziped directory" >> update.index.log
+printf '%s %s\n' "$(date)" " removed unziped directory" >> /Users/prabath/wso2is-repo-explorer/update.index.log
 cd ../git/wso2is-repos
 ./rex.sh update
-printf '%s %s\n' "$(date)" " git repos updated" >> update.index.log
+printf '%s %s\n' "$(date)" " git repos updated" >> /Users/prabath/wso2is-repo-explorer/update.index.log
 cp -r .repodata/wso2* ../wso2is-repo-explorer/src/indexes/
 cd ../wso2is-repo-explorer
 while read number; do
@@ -26,8 +26,8 @@ done <./src/indexes/version
 increment=1 
 version_new=$(($version_old + $increment))
 echo "$version_new" > ./src/indexes/version
-printf '%s %s\n' "$(date)" " version updated" >> update.index.log
+printf '%s %s\n' "$(date)" " version updated" >> /Users/prabath/wso2is-repo-explorer/update.index.log
 git add .
 git commit -m "automatic updates to indexes"
 git push
-printf '%s %s\n' "$(date)" " pushed to git" >> update.index.log
+printf '%s %s\n' "$(date)" " pushed to git" >> /Users/prabath/wso2is-repo-explorer/update.index.log
